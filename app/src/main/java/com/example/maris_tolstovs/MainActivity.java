9d9219cde1716dd7f868b38743523089bed5a6b0
mlirog <mlirog@users.github.com>
2020-05-17 03:38:12 +0300

    update

--- app/src/main/java/com/example/maris_tolstovs/MainActivity.java
+++ app/src/main/java/com/example/maris_tolstovs/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.maris_tolstovs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -10,10 +11,14 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,12 +28,6 @@ import java.io.IOException;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
 
     public final static String MODULE_MAC = "98:D3:51:F9:4E:78";
     public final static int REQUEST_ENABLE_BT = 1;
@@ -42,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
     EditText command_label;
     TextView response;
     Spinner command_list;
+    CheckBox dropdown_chk, manual_chk;
     public Handler mHandler;
+    ProgressDialog loading_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +51,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("[BLUETOOTH]", "Creating listeners");
-        response = (TextView) findViewById(R.id.response);
-        execute_command_button = (Button) findViewById(R.id.run_command);
+
+        //point where is every object in view
+        response = findViewById(R.id.response);
+        execute_command_button = findViewById(R.id.run_command);
         command_label = findViewById(R.id.command_field);
         clear_logs = findViewById(R.id.clear_logs);
         command_list = findViewById(R.id.command_list);
+        dropdown_chk = findViewById(R.id.chkbox_dropdown);
+        manual_chk = findViewById(R.id.chkbox_manual);
+
+        //opens loading dialog
+        loading_dialog = new ProgressDialog(MainActivity.this);
+        loading_dialog.setMessage(getString(R.string.loading_text));
+        loading_dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        loading_dialog.show();
+        loading_dialog.setCancelable(false);
 
         //load commands into spinner
         String[] items = new String[]{
@@ -66,20 +78,71 @@ public class MainActivity extends AppCompatActivity {
                 "a"
         };
 
-        //adapter for dropdown
+        //adapter for dropdown to show it in view
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         command_list.setAdapter(adapter);
 
+        command_label.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //empty required override method
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //on text change
+                //changes checkboxes to active option
+                if(!manual_chk.isChecked()){
+                    manual_chk.setChecked(true);
+                    dropdown_chk.setChecked(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //empty required override method
+            }
+        });
+
+        command_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //on option selection
+
+                //changes checkboxes to active option
+                if(!dropdown_chk.isChecked()){
+                    manual_chk.setChecked(false);
+                    dropdown_chk.setChecked(true);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //empty required override method
+            }
+        });
+
+        //run command buton onclick function
         execute_command_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("[BLUETOOTH]", "Attempting to send data");
-                if (mmSocket.isConnected() && btt != null) { //if we have connection to the bluetoothmodule
 
-                        String sendtxt = command_label.getText().toString();
-                        Log.i("[BLUETOOTH]", "Command:"+sendtxt);
-                        btt.write(sendtxt.getBytes());
-
+                //if we have connection to the bluetoothmodule
+                if (mmSocket.isConnected() && btt != null) {
+                        //if manual command mode is selected
+                        if(manual_chk.isChecked()){
+                            //reads entered command
+                            String sendtxt = command_label.getText().toString();
+                            Log.i("[BLUETOOTH]", "Command:"+sendtxt);
+                            btt.write(sendtxt.getBytes());
+                        //if dropdown commands checkbox is checked
+                        }else if(dropdown_chk.isChecked()){
+                            //gets items string from selected dropdown item
+                            String sendtxt = command_list.getSelectedItem().toString();
+                            Log.i("[BLUETOOTH]", "Command:"+sendtxt);
+                            btt.write(sendtxt.getBytes());
+                        }
                 } else {
                     Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
                 }
@@ -103,6 +166,8 @@ public class MainActivity extends AppCompatActivity {
         }else{
             initiateBluetoothProcess();
         }
+        //clos eloading
+        loading_dialog.dismiss();
     }
 
     @Override
